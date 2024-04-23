729865a297a956b15d38981e490a99d7947e9bba
NikolajWild <nime0002@stud.kea.dkgit config --global user.email NikolajWildgit config --global user.email nime0002@stud.kea.dk>
2024-04-23 14:34:53 +0200

    teeeeest

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dawg = new Dog();
+        Cat cawt = new Cat();
+        System.out.println(cawt.makeSound()+dawg.makeSound());
+    }
+
+
+}
